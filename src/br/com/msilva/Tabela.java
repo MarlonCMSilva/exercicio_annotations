a408d7d1146fc95d9a2c81ac45937a4aedaa50fa
MarlonCMSilva <marlon.conceicao.0308@gmail.com>
2024-08-20 09:43:20 -0300

    first commit

---
+++ src/br/com/msilva/Tabela.java
@@ -0,0 +1,12 @@
+package br.com.msilva;
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Tabela {
+
+    String product_name();
+    long product_id();
+    String value();
+}
---
+++ src/br/com/msilva/TabelaDeProdutos.java
@@ -0,0 +1,6 @@
+package br.com.msilva;
+
+public class TabelaDeProdutos {
+    @Tabela(product_name = "", product_id = 0L, value = "")
+    String name= "";
+}
